4a1924eac142f965605b5fa51de629439a9303ea
rahul <knr.rahul@gmail.com>
2018-10-08 22:06:53 -0500

    Added new repoaitory

---
+++ TestingProject/src/com/gautham/Practise/MyPractise/AppTest.java
@@ -0,0 +1,27 @@
+package com.gautham.Practise.MyPractise;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest{
+	@Test
+	public static void main() throws IOException
+	{
+		System.out.println("Execution started");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\jars\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in");
+		
+		System.out.println(driver.getTitle());
+		Assert.assertEquals(true, true);
+	}
+}
